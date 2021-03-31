78be05f3f36e3cdb643e636ca8610987d802bdf9
ezzedin779 <abdou.babchia64@gmail.com>
2021-03-31 22:27:55 +0100

    finished non_working

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This is the module that instantiates an object of class FileStorage"""
 from os import getenv
 
 
