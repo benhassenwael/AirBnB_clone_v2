3893a8f2629c11a975d6a6b7e440162f651c6838
benhassenwael <44639258+benhassenwael@users.noreply.github.com>
2021-04-18 18:03:09 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
 echo 'Test file' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
-insert='\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
+insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
 sed -i "/location \/redirect_me {/i $insert" /etc/nginx/sites-available/default
 service nginx restart
 exit 0
