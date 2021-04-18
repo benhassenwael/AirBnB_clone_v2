3d4ef31de97c2943ae686a52aafc89f4e81b39b1
benhassenwael <greenw2012@gmail.com>
2021-04-18 17:54:05 +0100

    deploy scripts

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Script using bash to setup webservers for deployment of simple HTML
+
+
+apt-get update 
+apt-get install -y nginx 
+mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
+echo 'Test file' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+insert='\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
+sed -i "/location \/redirect_me {/i $insert" /etc/nginx/sites-available/default
+service nginx restart
+exit 0
+
